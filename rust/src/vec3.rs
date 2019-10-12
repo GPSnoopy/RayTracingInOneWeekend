bd3e8ef0de14b7c5a7d1a4929d526a9bf6e28587
Tanguy Fautre <tanguy@fautre.com>
2019-10-12 15:19:50 +0100

    Added separate path for FMA when not supported by CPU.

--- rust/src/vec3.rs
+++ rust/src/vec3.rs
@@ -196,8 +196,11 @@ pub fn cross(l: Vec3, r: Vec3) -> Vec3 {
 pub fn dot(l: Vec3, r: Vec3) -> f32 {
     // For some reason, Rust 2018 does not automatically generate FMA instructions. 
     // It needs a bit of explicit help.
-    //l.x * r.x + l.y * r.y + l.z * r.z
-    l.x.mul_add(r.x, l.y.mul_add(r.y, l.z * r.z))
+    #[cfg(not(target_feature = "fma"))]
+    { l.x * r.x + l.y * r.y + l.z * r.z }
+    
+    #[cfg(target_feature = "fma")]
+    { l.x.mul_add(r.x, l.y.mul_add(r.y, l.z * r.z)) }
 }
 
 pub fn squared_length(v: Vec3) -> f32 {
