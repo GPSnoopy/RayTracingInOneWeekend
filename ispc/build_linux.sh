e0c867d1ceba3788d087c5d83620a53415802caa
Tanguy Fautre <tanguy@fautre.com>
2020-11-28 21:32:02 +0000

    Upgrade to ISPC 1.14.1

--- ispc/build_linux.sh
+++ ispc/build_linux.sh
@@ -3,5 +3,5 @@ set -e
 
 mkdir --parents build/linux
 cd build/linux
-cmake -D ISPC_EXECUTABLE=/home/gpsnoopy/development/ispc-v1.13.0-linux/bin/ispc ../..
+cmake -D ISPC_EXECUTABLE=/home/gpsnoopy/development/ispc-v1.14.1-linux/bin/ispc ../..
 make -j
--- ispc/build_windows.bat
+++ ispc/build_windows.bat
@@ -2,7 +2,7 @@ mkdir build
 cd build || goto :error
 mkdir windows || goto :error
 cd windows || goto :error
-cmake -D ISPC_EXECUTABLE=C:/Development/ispc-v1.13.0-windows/bin/ispc.exe -G "Visual Studio 16 2019" -A "x64" ../.. || goto :error
+cmake -D ISPC_EXECUTABLE=D:/SDKs/ISPC/ispc-v1.14.1-windows/bin/ispc.exe -G "Visual Studio 16 2019" -A "x64" ../.. || goto :error
 msbuild RayTracingInOneWeekend.sln /t:Rebuild /p:Configuration=Release || goto :error
 cd ..
 cd ..
