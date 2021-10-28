0c0dd48c24c6969daf735cb0f622555ad525f5eb
Tanguy Fautre <tanguy@fautre.com>
2021-10-28 15:27:47 +0100

    Update linux build script to ispc 1.16.1

--- ispc/build_linux.sh
+++ ispc/build_linux.sh
@@ -3,5 +3,5 @@ set -e
 
 mkdir --parents build/linux
 cd build/linux
-cmake -D ISPC_EXECUTABLE=/home/gpsnoopy/Development/ispc-v1.15.0-linux/bin/ispc -D X86_64_CPU=1 ../..
+cmake -D ISPC_EXECUTABLE=/home/gpsnoopy/Development/ispc-v1.16.1-linux/bin/ispc -D X86_64_CPU=1 ../..
 make -j
